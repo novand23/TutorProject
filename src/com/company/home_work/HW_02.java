49d416173ac5623b758d9869c5e6486565cc5309
novand23 <novand23@gmail.com>
2019-03-26 23:46:31 +0200

    init

---
+++ src/com/company/home_work/HW_02.java
@@ -0,0 +1,24 @@
+package com.company.home_work;
+
+public class HW_02 {
+    public static void main (String[]args){
+        int a = 100000;
+        byte b = 99;
+        short c = 28000;
+        char d = '@';
+        boolean world = true;
+        float e = 65.3f;
+        long f = 1000000000000L;
+        double g = 854.8;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(world);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+    }
+}
