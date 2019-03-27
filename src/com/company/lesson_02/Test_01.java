72ab671cee58aaddff87fee2db4a37b30ff5e12b
novand23 <novand23@gmail.com>
2019-03-27 22:01:09 +0200

    init

--- src/com/company/lesson_02/Test_01.java
+++ src/com/company/lesson_02/Test_01.java
@@ -1,4 +1,29 @@
 package com.company.lesson_02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Test_01 {
+
+    public static void main (String[] args) throws IOException {
+         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+         System.out.println("Enter a text: ");
+         String str = bufferedReader.readLine();
+
+         System.out.println("Enter a number: ");
+         String str2 = bufferedReader.readLine();
+         int number = Integer.parseInt(str2);
+
+         System.out.println("Enter a number: ");
+         int number2 = Integer.parseInt(bufferedReader.readLine());
+
+         System.out.println("Text: " + str);
+
+         System.out.println(number * 2);
+
+         System.out.println(number * number2);
+
+    }
 }
