ca96ef5bc1dd1424ef922487459323dfa2eaebfb
novand23 <novand23@gmail.com>
2019-03-27 21:19:12 +0200

    init

--- src/com/company/home_work/HW_01.java
+++ src/com/company/home_work/HW_01.java
@@ -2,11 +2,17 @@ package com.company.home_work;
 
 public class HW_01 {
     public static void main (String [] args){
-        int number1=10;
-        int number2=18;
-        int number3=21;
+        int number1=10; //18
+        int number2=18; //21
+        int number3=21; //10
+        int number4=number1;
+
+        number1=number2;
+        number2=number3;
+        number3=number4;
+
+        System.out.println(number1);
         System.out.println(number2);
         System.out.println(number3);
-        System.out.println(number1);
     }
 }
