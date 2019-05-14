4cbdb42f166a2a4bfe3daba9594ad3eb90f354a3
novand23 <novand23@gmail.com>
2019-05-14 20:48:50 +0300

    init

--- src/com/company/Lesson_16/Test_01.java
+++ src/com/company/Lesson_16/Test_01.java
@@ -32,6 +32,10 @@ public class Test_01 {
         for (int i = 0; i < list1.size(); i++) {
             System.out.println(list1.get(i));
         }
+        List<String> list2 = triplingValues(list);
+        for (int i = 0; i < list2.size(); i++) {
+            System.out.println(list2.get(i));
+        }
 
     }
 
@@ -44,5 +48,13 @@ public class Test_01 {
         }
         return list1;
     }
-
+    private static List<String> triplingValues(List<String> a) {
+        List<String> list2 = new ArrayList<>();
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i).length() % 2 != 0) {
+                list2.add(a.get(i) + " " + a.get(i)+ " " + a.get(i));
+            }
+        }
+        return list2;
+    }
 }
