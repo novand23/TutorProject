b21a879a3da03cd9f2c0ec8299ef2a85a15ab3e6
novand23 <novand23@gmail.com>
2019-06-04 21:30:47 +0300

    init

---
+++ src/com/company/Lesson_20_Collection_1/Test_20_03.java
@@ -0,0 +1,40 @@
+package com.company.Lesson_20_Collection_1;
+
+/* Удалить все числа больше 10
+Создать множество чисел(Set<Integer>), занести туда 5 различных чисел.
+При помощи метода  removeAllNumbersMoreThan10 удалить из множества все числа больше 10.
+*/
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Test_20_03 {
+    public static void main (String[]args){
+        removeAllNumbersMoreThan10(setnum());
+
+    }
+    private static Set<Integer> setnum(){
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(4);
+        set.add(5);
+        set.add(12);
+        set.add(9);
+        set.add(54);
+        set.add(90);
+        set.add(3);
+        return set;
+    }
+    private static void removeAllNumbersMoreThan10 (Set<Integer> set){
+        Iterator<Integer> it = set.iterator();
+        while (it.hasNext()){
+            int a = it.next();
+            if (a>10){
+                it.remove();
+            }else{
+                System.out.println(a);
+            }
+        }
+    }
+}
