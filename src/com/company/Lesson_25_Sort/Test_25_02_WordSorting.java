9deea234134e8c76765e0a0d4aa893f6e9ad0110
novand23 <novand23@gmail.com>
2019-06-19 23:22:04 +0300

    init

--- src/com/company/Lesson_25_Sort/Test_25_02_WordSorting.java
+++ src/com/company/Lesson_25_Sort/Test_25_02_WordSorting.java
@@ -8,15 +8,23 @@ public class Test_25_02_WordSorting {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] arr = new String[5];
-
         for (int i = 0; i < arr.length; i++) {
-            System.out.println("Введите число: ");
+            System.out.println("Введите строку: ");
             arr[i] = bf.readLine();
         }
+       sort(arr);
     }
 
-
-    private static void sort (String[] s) {
+    private static void sort(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            for (int a = i; a < words.length; a++)
+                if (words[a].compareTo(words[i]) < 0) {
+                    String k = words[a];
+                    words[a] = words[i];
+                    words[i] = k;
+                }
+            System.out.println(words[i]);
+        }
 
     }
 }
