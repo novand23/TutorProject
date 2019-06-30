dfadc1700dd8699ff3015e2ee45e961bf37961cb
novand23 <novand23@gmail.com>
2019-06-30 18:18:56 +0300

    init

---
+++ src/com/company/Lesson_28_OOP/HW_28_04.java
@@ -0,0 +1,38 @@
+package com.company.Lesson_28_OOP;
+
+/* Или «Корова», или «Кит», или «Собака», или «Неизвестное животное»
+Написать метод, который определяет, объект какого класса ему передали, и возвращает результат – одно значение из: «Корова», «Кит», «Собака», «Неизвестное животное».
+*/
+public class HW_28_04 {
+    public static void main(String[] args) {
+        mathod(new UnknownAnimal());
+    }
+
+    private static void mathod(Object o) {
+        if (o instanceof Cow) {
+            System.out.println("Корова");
+        } else if (o instanceof Whale) {
+            System.out.println("Кит");
+        } else if (o instanceof Dog3) {
+            System.out.println("Собака");
+        } else if (o instanceof UnknownAnimal) {
+            System.out.println("Неизвестное животное");
+        }
+    }
+}
+
+class Cow {
+
+}
+
+class Whale {
+
+}
+
+class Dog3 {
+
+}
+
+class UnknownAnimal {
+
+}
---
+++ src/com/company/Lesson_28_OOP/HW_28_05.java
@@ -0,0 +1,27 @@
+package com.company.Lesson_28_OOP;
+/* Создать класс Pet с методом getName, который возвращает строку "Я - пушистик"
+Создать класс Cat и унаследоваться от Pet
+Переопредели метод getName в классе Cat так, чтобы программа выдавала на экран надпись
+«Я - кот».
+*/
+public class HW_28_05 {
+    public static void main (String []args){
+        Pet1 cat2 = new Cat2();
+        cat2.getName();
+    }
+}
+
+class Pet1 {
+
+    public void getName (){
+        System.out.println("Я - пушистик");
+    }
+}
+
+class Cat2 extends Pet1{
+
+    @Override
+    public void getName() {
+        System.out.println("Я - кот");
+    }
+}
