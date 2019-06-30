91bf84bdc2932e1481329ac5b1c8a181c806388c
novand23 <novand23@gmail.com>
2019-06-30 23:04:20 +0300

    init

--- src/com/company/Lesson_21_Collection_2/Test_21_02.java
+++ src/com/company/Lesson_21_Collection_2/Test_21_02.java
@@ -4,8 +4,24 @@ package com.company.Lesson_21_Collection_2;
    Найти ряд Фиббоначи для 10
 */
 public class Test_21_02 {
-    public static void main (String [] args){
-        
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(fibonachhi(i));
+        }
+    }
+
+    private static int fibonachhi(int n){
+        if (n<=0) {
+            return 0;
+        } else if(n == 1){
+            return 1;
+        } else if(n == 2){
+            return 1;
+        } else {
+            return fibonachhi(n-1) + fibonachhi(n - 2);
+        }
+
+
     }
 }
 //TODO
