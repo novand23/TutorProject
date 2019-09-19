0a8eac3fff5c3e8decfaaff04568a868ea912629
novand23 <novand23@gmail.com>
2019-09-19 21:31:04 +0300

    Lesson_35

--- src/com/company/Lesson_35_OOP/Test_35_02/Test_35_02.java
+++ src/com/company/Lesson_35_OOP/Test_35_02/Test_35_02.java
@@ -1,4 +1,59 @@
 package com.company.Lesson_35_OOP.Test_35_02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+0. Создать метод print и перегрузить его для разных типов:
+- print( Double value ), должен выводит на экран надпись "Это тип Double, значение " + value
+- print( String value ), должен выводит на экран надпись "Это тип String, значение " + value
+- print( short value ), должен выводит на экран надпись "Это тип short, значение " + value
+- print( Integer value ), должен выводит на экран надпись "Это тип Integer, значение " + value
+1. Считать с консоли данные, пока не введено слово "exit".
+2. Для каждого значения, кроме "exit", вызвать метод print. Если значение:
+2.1. содержит точку '.', то вызвать метод print для Double;
+2.2. больше нуля, но меньше 128, то вызвать метод print для short;
+2.3. больше либо равно 128, то вызвать метод print для Integer;
+2.4. иначе, вызвать метод print для String.
+*/
 public class Test_35_02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();
+
+        try {
+            while () {
+
+                if (s.contains(".")) {
+                    print(s);
+                } else {
+                    int num = Integer.parseInt(s);
+                    if (num > 0 && num < 128) {
+                        print(num);
+                    } else if (num >= 128) {
+                        print(num);
+                    } else {
+                        print(s);
+                    }
+                }
+            }
+        } catch (NumberFormatException e){
+            print(s);
+        }
+
+    }
+
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+    public static void print(String value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+    public static void print(short value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+    public static void print(Integer value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
 }
