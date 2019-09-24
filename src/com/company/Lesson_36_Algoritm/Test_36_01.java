763d9ba325d15ca4b0ef7907c74f6b713056ba16
novand23 <novand23@gmail.com>
2019-09-24 20:26:58 +0300

    Lesson_36

---
+++ src/com/company/Lesson_36_Algoritm/Test_36_01.java
@@ -0,0 +1,25 @@
+package com.company.Lesson_36_Algoritm;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test_36_01 {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 6, 1, 4, 3, 6};
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int k = i+1; k < array.length; k++)
+                if (array[i] == array[k]) {
+                    i++;
+                } else {
+                    list.add(array[k]);
+                    k++;
+                }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
